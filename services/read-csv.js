317ac58e6ba254ada2c41c1d8f1be01c7bb9fa76
suhailamir <suhail.amir101@gmail.com>
2015-04-08 09:39:11 +0500

    rabbit-mq queue added

--- services/read-csv.js
+++ services/read-csv.js
@@ -1,7 +1,10 @@
 require('seneca')()
-  .use('../lib/read-csv')
-  .use(require('seneca-rabbitmq-transport'))
-  .listen(10203)
-  .ready(function(){
-    this.act({role:'read-csv',cmd:'return-json',file_path:'./public/test.csv'},console.log)
-  })
+    .use('../lib/read-csv')
+    .use(require('seneca-rabbitmq-transport'))
+    .listen(10203)
+    .client({
+        type: 'rabbitmq'
+    })
+    .ready(function() {
+        console.log('service is listening on port: 10203')
+    })
--- test/read-csv.test.js
+++ test/read-csv.test.js
@@ -1,13 +1,15 @@
-
 var assert = require('assert')
 
 var seneca = require('seneca')()
-      .use('../lib/read-csv.js')
+    .use('../lib/read-csv.js')
+    .use(require('seneca-rabbitmq-transport'))
 
-seneca.act({role:'read-csv',cmd:'return-json',file_path: './public/test.csv'},function(err,out){
-  assert.ok(null==err)
-  assert.equal("Space is only noise",out.data['row 1']["Track Title"])
-  console.log(out.data)
+seneca.act({
+    role: 'read-csv',
+    cmd: 'return-json',
+    file_path: './public/test.csv'
+}, function(err, out) {
+    assert.ok(null == err)
+    assert.equal("Space is only noise", out.data['row 1']["Track Title"])
 
 })
-
